76ca7c661f2b54c430ad288f57c3e513943744d9
Seba_vac <80701967+SebaVac@users.noreply.github.com>
2021-06-25 19:46:21 +0000

    Arreglos tablero, primer cambio

--- Tablero.c
+++ Tablero.c
@@ -12,30 +12,111 @@ void mostrar_talero(){
 
   switch(op){
     case 1:
+    printf("         |        |       ");
+    printf("   %c    |    2   |   3  ",);
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   6  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 2:
+    printf("         |        |       ");
+    printf("    1    |   %c   |   3  ",);
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   6  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 3:
+    printf("         |        |       ");
+    printf("    1    |    2   |   %c  ",);
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   6  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 4:
+    printf("         |        |       ");
+    printf("    1    |    2   |   3  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("   %c    |   5    |   6  ",);
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 5:
+    printf("         |        |       ");
+    printf("    1    |    2   |   3  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   %c   |   6  ",);
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 6:
+    printf("         |        |       ");
+    printf("    1    |    2   |   3  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   %c  ",);
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 7:
+    printf("         |        |       ");
+    printf("    1    |    2   |   3  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   6  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    %c   |    8   |   9  ");
+    printf("         |        |       ");
     break;
 
-    case 8: 
+    case 8:
+    printf("         |        |       ");
+    printf("    1    |    2   |   3  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   6  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |   %c   |   9  ");
+    printf("         |        |       ");
     break;
 
     case 9:
+    printf("         |        |       ");
+    printf("    1    |    2   |   3  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    4    |   5    |   6  ");
+    printf("_________+________+_______");
+    printf("         |        |       ");
+    printf("    7    |    8   |   %c  ",);
+    printf("         |        |       ");
     break;
   }
 
